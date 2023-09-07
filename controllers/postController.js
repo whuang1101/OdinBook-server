119603252c6f936551ddc87074067a0520ea6362
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-09-06 20:46:21 -0400

    Add like and remove like

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,8 @@
  const asyncHandler = require("express-async-handler");
  const User = require("../models/user");
- const Post = require("../models/post")
+ const Post = require("../models/post");
+const user = require("../models/user");
+const mongoose = require("mongoose")
  //this allows post to be made
  module.exports.post = asyncHandler(async(req,res,next) => {
     const findUser = await User.findById(req.body.userId);
@@ -18,26 +20,38 @@
                 findUser._id, 
                 { $push: { posts: save._id } 
             })
-            console.log("post saved and added to user posts")
+            res.status(200);
             }
+    }
+    else{
+        res.status(404);
     }
 })
 
 // this makes it so that you can get all posts either public/your friends or your own
 module.exports.get = asyncHandler(async(req,res,next) => {
     const userId = req.params.userId;
-    const findUser = await User.findById(userId)
-        .populate({
-            path: 'posts',
-            populate: {
-                path: 'author',
-                model: 'Users' 
-            }
-        });
 
-    let posts = [];
-    if(findUser){
-        posts = posts.concat(findUser.posts);
-    }
+    // retrieving personal posts
+    let posts = await Post.find({author: userId}).populate("author").sort({date: -1});
     res.status(200).json(posts);
+})
+
+// adds a like to a post
+module.exports.addLike = asyncHandler(async(req,res,next) => {
+    const postId = req.body.postId;
+    const userId = req.body.userId;
+
+    const findPost = await Post.findByIdAndUpdate(postId, {$push:{likes:userId}})
+    if(findPost){
+        res.status(200)
+    }
+})
+module.exports.removeLike = asyncHandler(async(req,res,next) => {
+    const postId = req.body.postId;
+    const userId = req.body.userId;
+    const findPost = await Post.findByIdAndUpdate(postId, {$pull:{likes:userId}})
+    if(findPost){
+        res.status(200)
+    }
 })
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const postController = require("../controllers/postController")
 router.get("/:userId", postController.get);
 router.post("/", postController.post);
+router.put("/like/add", postController.addLike)
+router.put("/like/remove", postController.removeLike)
 
 module.exports = router;
