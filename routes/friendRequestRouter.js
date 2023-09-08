4fc0835dab86d78842288b55299190037156ab48
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-09-07 21:03:30 -0400

    View all frineds

--- routes/friendRequestRouter.js
+++ routes/friendRequestRouter.js
@@ -4,10 +4,8 @@ const friendRequestController = require("../controllers/friendRequestController"
 router.get("/suggestions/:userId", friendRequestController.getSuggestions);
 router.get("/pending/:id", friendRequestController.getPending);
 router.get("/request/:id", friendRequestController.getRequests);
-
+router.get("/:id", friendRequestController.getAllFriends)
 router.post("/add", friendRequestController.addFriend)
 router.post("/remove", friendRequestController.removeRequest)
-
 router.put("/request/accept", friendRequestController.acceptRequest)
-
 module.exports = router;
